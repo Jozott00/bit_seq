49272844b3d0778c8f3d2e0fac4e278bddd2c318
Johannes Zottele <official@johannes-zottele.at>
2023-06-03 12:12:46 +0200

    refactor into separate module

---
+++ src/bit_seq_input.rs
@@ -0,0 +1,103 @@
+use syn::{ExprLit, ExprPath, LitInt, Result, Token};
+use syn::parse::{Parse, ParseStream};
+
+pub struct BitSeqInput {
+    bit_segments: Vec<BitSegment>,
+}
+
+pub enum BitSegment {
+    Bits(syn::LitInt),
+    Expr(syn::Expr, syn::LitInt),
+}
+
+impl BitSeqInput {
+    pub fn segments(&self) -> &Vec<BitSegment> {
+        &self.bit_segments
+    }
+
+    fn parse_expr(input: &ParseStream) -> Result<BitSegment> {
+        let val = if input.peek(syn::Ident) {
+            let ident = input.parse::<syn::Ident>()?;
+            syn::Expr::Path(ExprPath {
+                attrs: vec![],
+                qself: None,
+                path: ident.into(),
+            })
+        } else {
+            let num = input.parse::<syn::LitInt>()?;
+            syn::Expr::Lit(
+                ExprLit {
+                    attrs: vec![],
+                    lit: syn::Lit::Int(num),
+                })
+        };
+
+        if !input.peek(Token![:]) {
+            return Err(input.error("expected `:`"));
+        }
+
+        input.parse::<Token![:]>()?;
+
+        if !input.peek(syn::LitInt) {
+            return Err(input.error("expected integer that specifies size of bit sequence"));
+        }
+
+        let size = input.parse::<syn::LitInt>()?;
+        Ok(BitSegment::Expr(val, size))
+    }
+
+    fn parse_bits(input: &ParseStream) -> Result<BitSegment> {
+        let num = input.parse::<syn::LitInt>()?;
+        let num_string = num.to_string();
+
+        if num_string.starts_with('-') {
+            return Err(syn::Error::new(num.span(), "negative numbers are not allowed"));
+        }
+
+        // check for hexadecimal literal
+        if num_string.starts_with("0x") {
+            let bit_len = (num_string.len() - 2) * 4;
+            let lit_len = LitInt::new(&bit_len.to_string(), num.span());
+
+            let expr = syn::Expr::Lit(
+                ExprLit {
+                    attrs: vec![],
+                    lit: syn::Lit::Int(num),
+                });
+
+            return Ok(BitSegment::Expr(expr, lit_len));
+        }
+
+        // check for binary literal
+        let is_binary = num_string.chars().all(|c| c == '0' || c == '1');
+        if !is_binary {
+            let err = format!("expected bit sequence but got integer instead.");
+            return Err(syn::Error::new(num.span(), err));
+        }
+        Ok(BitSegment::Bits(num))
+    }
+}
+
+impl Parse for BitSeqInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut bit_segments = Vec::new();
+
+        while !input.is_empty() {
+            if input.peek(syn::Ident) || (input.peek(syn::LitInt) && input.peek2(Token![:])) {
+                let segment = BitSeqInput::parse_expr(&input)?;
+                bit_segments.push(segment);
+            } else if input.peek(syn::LitInt) {
+                let segment = BitSeqInput::parse_bits(&input)?;
+                bit_segments.push(segment);
+            }
+            // parse an expression segment
+            else {
+                return Err(input.error("expected bit sequence, hex or length defined expression"));
+            }
+        }
+
+        Ok(BitSeqInput {
+            bit_segments,
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,138 +1,116 @@
-use proc_macro::{TokenStream};
-use syn::parse::{Parse, ParseStream};
-use syn::{parse_macro_input, Token, Result, LitInt, ExprLit, ExprPath};
-use quote::{quote, quote_spanned};
+use proc_macro::TokenStream;
+
 use proc_macro_error::*;
+use quote::{quote, quote_spanned};
+use syn::{ExprLit, ExprPath, LitInt, parse_macro_input, Result, Token};
+use syn::__private::TokenStream2;
 use syn::spanned::Spanned;
+use crate::bit_seq_input::{BitSegment::{self, *}, BitSeqInput};
 
-struct BitSeqInput {
-    bit_segments: Vec<BitSegment>,
-}
-
-enum BitSegment {
-    Bits(syn::LitInt),
-    Expr(syn::Expr, syn::LitInt),
-}
-
-impl Parse for BitSeqInput {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let mut bit_segments = Vec::new();
-        let mut last_span = input.span();
-        while !input.is_empty() {
-            if input.peek(syn::Ident) || (input.peek(syn::LitInt) && input.peek2(Token![:])) {
-                let val = if input.peek(syn::Ident) {
-                    let ident = input.parse::<syn::Ident>()?;
-                    syn::Expr::Path(ExprPath {
-                        attrs: vec![],
-                        qself: None,
-                        path: ident.into(),
-                    })
-                } else {
-                    let num = input.parse::<syn::LitInt>()?;
-                    syn::Expr::Lit(
-                        ExprLit {
-                            attrs: vec![],
-                            lit: syn::Lit::Int(num),
-                        })
-                };
-
-                if !input.peek(Token![:]) {
-                    return Err(input.error("expected `:`"));
-                }
-
-                input.parse::<Token![:]>()?;
-
-                if !input.peek(syn::LitInt) {
-                    return Err(input.error("expected integer that specifies size of bit sequence"));
-                }
-
-                let size = input.parse::<syn::LitInt>()?;
-                let segment = BitSegment::Expr(val, size);
-                bit_segments.push(segment);
-            } else if input.peek(syn::LitInt) {
-                let num = input.parse::<syn::LitInt>()?;
-                let num_string = num.to_string();
-
-                // check for hexadecimal literal
-                if num_string.starts_with("0x") {
-                    let bit_len = (num_string.len() - 2) * 4;
-                    let lit_len = LitInt::new(&bit_len.to_string(), num.span());
-
-                    let expr = syn::Expr::Lit(
-                        ExprLit {
-                            attrs: vec![],
-                            lit: syn::Lit::Int(num),
-                        });
-
-                    bit_segments.push(BitSegment::Expr(expr, lit_len));
-                    continue;
-                }
-
-                // check for binary literal
-                let is_binary = num_string.chars().all(|c| c == '0' || c == '1');
-                if !is_binary {
-                    let err = format!("expected bitsequence but got integer instead.");
-                    return Err(syn::Error::new(last_span, err));
-                }
-                bit_segments.push(BitSegment::Bits(num))
-            }
-            // parse an expression segment
-            else {
-                return Err(input.error("expected bit sequence, hex or length defined expression"));
-            }
-
-            // update the previous token cursor
-            last_span = input.span();
-        }
-        Ok(BitSeqInput {
-            bit_segments,
-        })
-    }
-}
+mod bit_seq_input;
 
 
+/// `bseq` is a procedural macro for creating bit sequences.
+///
+/// This macro enables the generation of bit sequences using a simple syntax.
+/// Bit sequences can be specified directly, through hex values, or by using identifiers or integers
+/// each with a specific length. This proves especially useful in systems programming and when interacting
+/// with low-level hardware or protocols where bit manipulation is a common requirement.
+///
+/// # Examples
+///
+/// #### Direct raw bit sequence:
+/// ```
+/// use bit_seq::bseq;
+///
+/// let t = bseq!(0110 01 0 1);
+/// assert_eq!(t, 0b0110_01_0_1);
+/// ```
+///
+/// #### Using hex values:
+///
+/// Hex values conveniently add 4 bits per hexadecimal place.
+/// ```
+/// use bit_seq::bseq;
+///
+/// let t = bseq!(01 0x1f);
+/// assert_eq!(t, 0b01_0001_1111);
+/// ```
+///
+/// #### Using value length expression:
+///
+/// Employ the format `<val>:<len>` where `len` specifies how many of the
+/// least significant bits from `val` should be used.
+/// ```
+/// use bit_seq::bseq;
+///
+/// let t = bseq!(3:1 0 0xf:2);
+/// assert_eq!(t, 0b1_0_11);
+/// ```
+///
+/// #### Using variable length expression:
+///
+/// It is also possible to interpolate outer variables for length expressions.
+/// ```
+/// use bit_seq::bseq;
+/// let var = 0xf;
+/// let t = bseq!(10 var:2);
+/// assert_eq!(t, 0b10_11);
+/// ```
+///
+/// Note: Since the macros are compiled into common bit manipulation operations,
+/// the usage of this macro doesn't introduce additional runtime overhead.
+///
+/// The macro outputs a numerical value with the appropriate bits set, providing an
+/// efficient method to generate specific bit sequences.
 #[proc_macro]
 #[proc_macro_error]
 pub fn bseq(input: TokenStream) -> TokenStream {
-    use BitSegment::*;
-
+    // parse input
     let input = parse_macro_input!(input as BitSeqInput);
 
     // construct shift token streams
     let mut bit_len = 0;
-    let shifts: Vec<_> = input.bit_segments.iter().rev().map(|seq| {
-        let (val, len) = match seq {
-            Bits(bits) => {
-                let b = bits.to_string();
-                let num = usize::from_str_radix(&b, 2).unwrap();
-                let num_lit = LitInt::new(&num.to_string(), b.span());
-                let span = bits.span();
-                let rep = quote_spanned!(span=> #num_lit);
-                (rep, b.len())
-            }
-            Expr(expr, len_lit) => {
-                let len: usize = len_lit.base10_parse().unwrap_or_else(|_| abort!(len_lit, "Couldn't be parsed!"));
-                let mask: u128 = (1 << len) - 1;
-                let mask_lit = LitInt::new(&mask.to_string(), expr.span());
-                let span = expr.span();
-                let rep = quote_spanned!(span=> #expr & #mask_lit);
-                (rep, len)
-            }
-        };
+    let shifts: Vec<_> = input.segments()
+        .iter().rev()
+        .map(|seg| map_segment(seg, &mut bit_len))
+        .collect();
 
-        let span = val.span();
-        let bit_len_lit = LitInt::new(&bit_len.to_string(), span);
-        let res = quote_spanned!(span=> (#val) << #bit_len_lit);
-        bit_len += len;
-        res
-    }).collect();
-
+    // combine all shift segments
     let span = proc_macro2::Span::call_site();
     let mut macro_out = quote!(#(#shifts)|*);
     if macro_out.is_empty() {
+        // if no input provided, result is 0
         macro_out = quote_spanned!(span=> 0);
     }
 
     macro_out.into()
 }
 
+
+fn map_segment(seg: &BitSegment, curr_bit_len: &mut usize) -> TokenStream2 {
+    let (val, len) = match seg {
+        Bits(bits) => {
+            let b = bits.to_string();
+            let num = usize::from_str_radix(&b, 2).unwrap();
+            let num_lit = LitInt::new(&num.to_string(), b.span());
+            let span = bits.span();
+            let rep = quote_spanned!(span=> #num_lit);
+            (rep, b.len())
+        }
+        Expr(expr, len_lit) => {
+            let len: usize = len_lit.base10_parse().unwrap_or_else(|_| abort!(len_lit, "Couldn't be parsed!"));
+            let mask: u128 = (1 << len) - 1;
+            let mask_lit = LitInt::new(&mask.to_string(), expr.span());
+            let span = expr.span();
+            let rep = quote_spanned!(span=> #expr & #mask_lit);
+            (rep, len)
+        }
+    };
+
+    let span = val.span();
+    let bit_len_lit = LitInt::new(&curr_bit_len.to_string(), span);
+    let res = quote_spanned!(span=> (#val) << #bit_len_lit);
+    *curr_bit_len += len;
+    res
+}
